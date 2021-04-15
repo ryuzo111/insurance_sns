997782bdce1bc00548e1c50bd6484a581972ae0c
ryuzo111 <yajima0401ryuzo@gmail.com>
2021-04-15 23:46:58 +0900

    view変更

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -6,17 +6,29 @@
 <form action="{{route('post.post')}}" method="post">
 
 {{csrf_field()}}
+<div class="post-container1">
 <p>悩みのタイトル</p>
-<input type="text" name="title" placeholder="タイトルを入力してください" required>
+@if ($errors->has('title'))
+{{$errors->first('title')}}
+@endif
+<input class="post-title" type="text" name="title" placeholder="タイトルを入力してください" required>
+</div>
 
-<p>どんなタイプの悩み
+<div class="post-container2">
+@if ($errors->has('trouble'))
+{{$errors->first('trouble')}}
+@endif
+<p class="post-container2-p">どんなタイプの悩み</br>
 <input type="radio" name="trouble" value=1>保険加入についての悩み
 <input type="radio" name="trouble" value=2>現在加入している保険の悩み
 <input type="radio" name="trouble" value=3>健康告知についての悩み
 <input type="radio" name="trouble" value=4>その他の悩み
 </p>
+</div>
 
-<p>もし保険を探しているなら、どんな保険を探している?※探している人のみ
+
+<div class="post-container2">
+<p>もし保険を探しているなら、どんな保険を探している?※探している人のみ</br>
 <input type="checkbox" name="life" value=1>生命保険
 <input type="checkbox" name="medical" value=1>医療保険
 <input type="checkbox" name="saving" value=1>貯蓄タイプの保険
@@ -25,12 +37,26 @@
 <input type="checkbox" name="all_life" value=1>終身保険
 <input type="checkbox" name="other" value=1>その他の保険
 </p>
+</div>
 
+
+<div class="post-container3">
+@if ($errors->has('insurance_value'))
+{{$errors->first('insurance_value')}}
+@endif
 <p>誰が何の保険にどのくらい加入したいのか？※答えられる人のみ</p>
-<input type="text" name="insurance_value" placeholder="旦那の生命保険を500万円備えたい">
+<input class="post-text" type="text" name="insurance_value" placeholder="旦那の生命保険を500万円備えたい">
+</div>
 
+<div class="post-container3">
+@if ($errors->has('contents'))
+{{$errors->first('contents')}}
+@endif
 <p>どんな悩み？自由に入力してください</p>
-<input type="text" name="contents" placeholder="子供が生まれました。保険に入るべきなのか？また、入ったほうが良いならどんな保険に入ると良いのか？教えて下さい" required>
+<input class="post-text" type="text" name="contents" placeholder="子供が生まれました。保険に入るべきなのか？また、入ったほうが良いならどんな保険に入ると良いのか？教えて下さい" required>
+</div>
 
+<div class="post-container3">
 <input type="submit" value="悩みを共有してアドバイスをもらう">
+</div>
 @endsection
