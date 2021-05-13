4ecae9d6c2c5e18dad80bec5bb16d1eb63f28d6f
ryuzo111 <yajima0401ryuzo@gmail.com>
2021-05-13 23:53:06 +0900

    goodカラムにdefaultを追加

--- database/migrations/2021_03_29_133212_add_good_to_comment_table.php
+++ database/migrations/2021_03_29_133212_add_good_to_comment_table.php
@@ -14,7 +14,7 @@ class AddGoodToCommentTable extends Migration
     public function up()
     {
         Schema::table('comments', function (Blueprint $table) {
-								$table->integer('good');
+								$table->integer('good')->default(0);
             //
         });
     }
